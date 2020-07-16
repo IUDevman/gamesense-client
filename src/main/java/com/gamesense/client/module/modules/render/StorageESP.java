b28a3a359db6f72cfaca3ef9467c280a37251429
IUDevman <58370012+IUDevman@users.noreply.github.com>
2020-07-16 10:41:47 -0500

    GameSense v2.0

--- src/main/java/com/gamesense/client/module/modules/render/StorageESP.java
+++ src/main/java/com/gamesense/client/module/modules/render/StorageESP.java
@@ -32,24 +32,23 @@ public class StorageESP extends Module {
     }
 
     public void onWorldRender(RenderEvent event){
-        Color c1 = new Color(200, 100, 0, (int)a.getValue());
-        Color c2 = new Color(200, 0, 200, (int)a.getValue());
+        Color c1 = new Color(255, 255, 0, (int)a.getValue());
+        Color c2 = new Color(100, 0, 100, (int)a.getValue());
         Color c3 = new Color(150, 150, 150, (int)a.getValue());
+        Color c4 = new Color(255,0,0, (int)a.getValue());
         if(chests != null && chests.size() > 0){
             GameSenseTessellator.prepareGL();
             chests.forEach((c, t)->{
                 if(mc.world.loadedTileEntityList.contains(c)) {
-                    if(c instanceof TileEntityChest
-                            || c instanceof TileEntityShulkerBox)
-                        //OsirisTessellator.drawBox(c.getPos(), c1.getRGB(), GeometryMasks.Quad.ALL);
-                        GameSenseTessellator.drawBoundingBox(mc.world.getBlockState(c.getPos()).getSelectedBoundingBox(mc.world, c.getPos()), (float)w.getValue(), c1.getRGB());
+                    if(c instanceof TileEntityChest)
+                            GameSenseTessellator.drawBoundingBox(mc.world.getBlockState(c.getPos()).getSelectedBoundingBox(mc.world, c.getPos()), (float)w.getValue(), c1.getRGB());
                     if(c instanceof TileEntityEnderChest)
-                        //OsirisTessellator.drawBox(c.getPos(), c2.getRGB(), GeometryMasks.Quad.ALL);
                         GameSenseTessellator.drawBoundingBox(mc.world.getBlockState(c.getPos()).getSelectedBoundingBox(mc.world, c.getPos()), (float)w.getValue(), c2.getRGB());
+                    if(c instanceof TileEntityShulkerBox)
+                        GameSenseTessellator.drawBoundingBox(mc.world.getBlockState(c.getPos()).getSelectedBoundingBox(mc.world, c.getPos()), (float)w.getValue(), c4.getRGB());
                     if(c instanceof TileEntityDispenser
                             || c instanceof TileEntityFurnace
                             || c instanceof TileEntityHopper)
-                        //OsirisTessellator.drawBox(c.getPos(), c3.getRGB(), GeometryMasks.Quad.ALL);
                         GameSenseTessellator.drawBoundingBox(mc.world.getBlockState(c.getPos()).getSelectedBoundingBox(mc.world, c.getPos()), (float)w.getValue(), c3.getRGB());
                 }
             });
