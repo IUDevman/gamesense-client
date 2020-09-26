c84fe90441e12fe23e4117aa73ed46123b5aee37
IUDevman <58370012+IUDevman@users.noreply.github.com>
2020-09-25 20:59:31 -0500

    Blink and credits update (thanks dngerr)

--- src/main/java/com/gamesense/client/module/modules/movement/Blink.java
+++ src/main/java/com/gamesense/client/module/modules/movement/Blink.java
@@ -1,8 +1,10 @@
 package com.gamesense.client.module.modules.movement;
 
+import com.gamesense.api.settings.Setting;
 import com.gamesense.client.GameSenseMod;
 import com.gamesense.api.event.events.PacketEvent;
 import com.gamesense.client.module.Module;
+import com.mojang.realmsclient.gui.ChatFormatting;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import net.minecraft.client.entity.EntityOtherPlayerMP;
@@ -16,11 +18,20 @@ public class Blink extends Module {
     public Blink() {
         super("Blink", Category.Movement);
     }
+
+    Setting.Boolean ghostPlayer;
+
+    public void setup(){
+        ghostPlayer = registerBoolean("Ghost Player", "GhostPlayer", true);
+    }
+
     EntityOtherPlayerMP entity;
     private final Queue<Packet> packets = new ConcurrentLinkedQueue();
+
     @EventHandler
     private Listener<PacketEvent.Send> packetSendListener = new Listener<>(event -> {
         Packet packet = event.getPacket();
+
         if (packet instanceof CPacketChatMessage || packet instanceof CPacketConfirmTeleport || packet instanceof CPacketKeepAlive || packet instanceof CPacketTabComplete || packet instanceof CPacketClientStatus) {
             return;
         }
@@ -30,27 +41,34 @@ public class Blink extends Module {
 
     public void onEnable() {
         GameSenseMod.EVENT_BUS.subscribe(this);
-        entity = new EntityOtherPlayerMP(mc.world, mc.getSession().getProfile());
-        entity.copyLocationAndAnglesFrom(mc.player);
-        entity.rotationYaw = mc.player.rotationYaw;
-        entity.rotationYawHead = mc.player.rotationYawHead;
-        mc.world.addEntityToWorld(666, entity);
+
+        if (ghostPlayer.getValue()) {
+            entity = new EntityOtherPlayerMP(mc.world, mc.getSession().getProfile());
+            entity.copyLocationAndAnglesFrom(mc.player);
+            entity.rotationYaw = mc.player.rotationYaw;
+            entity.rotationYawHead = mc.player.rotationYawHead;
+            mc.world.addEntityToWorld(667, entity);
+        }
     }
 
     public void onDisable() {
         GameSenseMod.EVENT_BUS.unsubscribe(this);
+
         if (this.entity != null) {
             mc.world.removeEntity(entity);
         }
-        if (this.packets.size() > 0) {
-            for (Packet packet : this.packets) {
+
+        if (packets.size() > 0) {
+            for (Packet packet : packets) {
                 mc.player.connection.sendPacket(packet);
             }
-            this.packets.clear();
+            packets.clear();
         }
     }
 
     public String getHudInfo(){
-        return "" + packets.size();
+        String t = "[" + ChatFormatting.WHITE + packets.size() + ChatFormatting.GRAY + "]";
+
+        return t;
     }
 }
