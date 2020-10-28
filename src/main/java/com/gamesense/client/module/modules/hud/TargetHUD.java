dfffd27ce3fcd26c57871b8a6bd2831b8fefa63d
linustouchtips <68214996+linustouchtips@users.noreply.github.com>
2020-10-28 12:35:13 -0700

    updated player model for targethud

--- src/main/java/com/gamesense/client/module/modules/hud/TargetHUD.java
+++ src/main/java/com/gamesense/client/module/modules/hud/TargetHUD.java
@@ -126,7 +126,7 @@ public class TargetHUD extends Module {
         GlStateManager.disableTexture2D();
         GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
 
-        GuiInventory.drawEntityOnScreen(x, y, 43, 28, 60, entityPlayer);
+        drawEntityOnScreen(x, y, 43, 28, 60, entityPlayer);
 
         GlStateManager.enableRescaleNormal();
         GlStateManager.enableTexture2D();
@@ -235,6 +235,35 @@ public class TargetHUD extends Module {
         else {
             healthColor = new GSColor(255, 0, 0, 255);
         }
+    }
+    
+    public static void drawEntityOnScreen(int posX, int posY, int scale, float mouseX, float mouseY, EntityLivingBase ent) {
+        GlStateManager.enableColorMaterial();
+        GlStateManager.pushMatrix();
+        GlStateManager.translate((float)posX, (float)posY, 50.0F);
+        GlStateManager.scale((float)(-scale), (float)scale, (float)scale);
+        GlStateManager.rotate(180.0F, 0.0F, 0.0F, 1.0F);
+        float f = ent.renderYawOffset;
+        float f1 = ent.rotationYaw;
+        float f2 = ent.rotationPitch;
+        float f3 = ent.prevRotationYawHead;
+        float f4 = ent.rotationYawHead;
+        GlStateManager.rotate(135.0F, 0.0F, 1.0F, 0.0F);
+        RenderHelper.enableStandardItemLighting();
+        GlStateManager.rotate(-135.0F, 0.0F, 1.0F, 0.0F);
+        GlStateManager.rotate(-((float)Math.atan((double)(mouseY / 40.0F))) * 20.0F, 1.0F, 0.0F, 0.0F);
+        GlStateManager.translate(0.0F, 0.0F, 0.0F);
+        RenderManager rendermanager = Minecraft.getMinecraft().getRenderManager();
+        rendermanager.setPlayerViewY(180.0F);
+        rendermanager.setRenderShadow(false);
+        rendermanager.renderEntity(ent, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F, false);
+        rendermanager.setRenderShadow(true);
+        GlStateManager.popMatrix();
+        RenderHelper.disableStandardItemLighting();
+        GlStateManager.disableRescaleNormal();
+        GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+        GlStateManager.disableTexture2D();
+        GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
     }
 
     private boolean IsValidEntity (Entity e){
