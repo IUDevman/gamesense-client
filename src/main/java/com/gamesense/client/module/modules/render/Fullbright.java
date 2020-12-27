52ad88341847934a45404465b3291c1020fdebd9
IUDevman <58370012+IUDevman@users.noreply.github.com>
2020-12-27 05:06:02 -0600

    fullbright cleanup

--- src/main/java/com/gamesense/client/module/modules/render/Fullbright.java
+++ src/main/java/com/gamesense/client/module/modules/render/Fullbright.java
@@ -7,39 +7,41 @@ import net.minecraft.potion.PotionEffect;
 
 import java.util.ArrayList;
 
-public class Fullbright extends Module{
-	public Fullbright(){
+public class Fullbright extends Module {
+
+	public Fullbright() {
 		super("Fullbright", Category.Render);
 	}
 
-	float oldGamma;
-	Setting.Mode Mode;
+	Setting.Mode mode;
 
-	public void setup(){
+	public void setup() {
 		ArrayList<String> modes = new ArrayList<>();
 		modes.add("Gamma");
 		modes.add("Potion");
 
-		Mode = registerMode("Mode", "Mode", modes, "Gamma");
+		mode = registerMode("Mode", "Mode", modes, "Gamma");
 	}
 
-	public void onEnable(){
+	float oldGamma;
+
+	public void onEnable() {
 		oldGamma = mc.gameSettings.gammaSetting;
 	}
 
-	public void onUpdate(){
-		if (Mode.getValue().equalsIgnoreCase("Gamma")){
+	public void onUpdate() {
+		if (mode.getValue().equalsIgnoreCase("Gamma")) {
 			mc.gameSettings.gammaSetting = 666f;
 			mc.player.removePotionEffect(Potion.getPotionById(16));
 		}
-		else if (Mode.getValue().equalsIgnoreCase("Potion")){
+		else if (mode.getValue().equalsIgnoreCase("Potion")) {
 			final PotionEffect potionEffect = new PotionEffect(Potion.getPotionById(16), 123456789, 5);
 			potionEffect.setPotionDurationMax(true);
 			mc.player.addPotionEffect(potionEffect);
 		}
 	}
 
-	public void onDisable(){
+	public void onDisable() {
 		mc.gameSettings.gammaSetting = oldGamma;
 		mc.player.removePotionEffect(Potion.getPotionById(16));
 	}
