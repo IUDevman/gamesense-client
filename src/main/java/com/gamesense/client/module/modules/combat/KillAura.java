47988ce0891994fe3e5cfd347c5a846422ea556d
0b00101010 <0b00101010@users.noreply.github.com>
2021-02-07 19:08:27 +0000

    Updated KillAura
    Added Sword/Axe/All support.
    Added Auto Switch which chooses best sword (genenrically).
    Added Prioritise Sword that chooses sword over axe.
    Auto Switch only activates if there is an enemy nearby to attack and above a certain health requirement.

--- src/main/java/com/gamesense/client/module/modules/combat/KillAura.java
+++ src/main/java/com/gamesense/client/module/modules/combat/KillAura.java
@@ -1,19 +1,27 @@
 package com.gamesense.client.module.modules.combat;
 
 import com.gamesense.api.event.events.PacketEvent;
-import com.gamesense.api.util.player.friend.Friends;
 import com.gamesense.api.setting.Setting;
+import com.gamesense.api.util.misc.Pair;
+import com.gamesense.api.util.player.InventoryUtil;
+import com.gamesense.api.util.player.friend.Friends;
 import com.gamesense.api.util.world.EntityUtil;
 import com.gamesense.client.GameSense;
 import com.gamesense.client.module.Module;
 import com.gamesense.client.module.ModuleManager;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.passive.EntityTameable;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemAxe;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.network.play.client.CPacketUseEntity;
@@ -21,8 +29,15 @@ import net.minecraft.util.EnumHand;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+/**
+ * @author GameSense client for original code (actual author unknown)
+ * @source https://github.com/IUDevman/gamesense-client/blob/2.2.6/src/main/java/com/gamesense/client/module/modules/combat/KillAura.java
+ * @author 0b00101010
+ * @since 07/02/2021
+ */
 public class KillAura extends Module {
 
 	public KillAura() {
@@ -32,17 +47,27 @@ public class KillAura extends Module {
 	Setting.Boolean players;
 	Setting.Boolean hostileMobs;
 	Setting.Boolean passiveMobs;
-	Setting.Boolean swordOnly;
+	Setting.Boolean sword;
+	Setting.Boolean axe;
+	Setting.Boolean all;
+	Setting.Boolean autoSwitch;
+	Setting.Boolean swordPriority;
 	Setting.Boolean caCheck;
 	Setting.Boolean criticals;
 	Setting.Double range;
+	Setting.Double switchHealth;
 
 	public void setup() {
 		players = registerBoolean("Players", true);
 		passiveMobs = registerBoolean("Animals", false);
 		hostileMobs = registerBoolean("Monsters", false);
 		range = registerDouble("Range", 5,0,10);
-		swordOnly = registerBoolean("Sword Only",true);
+		sword = registerBoolean("Sword",true);
+		axe = registerBoolean("Axe", false);
+		all = registerBoolean("All", false);
+		autoSwitch = registerBoolean("Switch", false);
+		switchHealth = registerDouble("Min Switch Health", 0f, 0f, 20f);
+		swordPriority = registerBoolean("Prioritise Sword", true);
 		criticals = registerBoolean("Criticals",true);
 		caCheck = registerBoolean("AC Check",false);
 	}
@@ -51,22 +76,43 @@ public class KillAura extends Module {
 
 	public void onUpdate() {
 		if (mc.player == null || mc.player.isDead) return;
+
+		final double rangeSq = range.getValue() * range.getValue();
 		List<Entity> targets = mc.world.loadedEntityList.stream()
+				.filter(entity -> !(entity instanceof EntityLivingBase))
 				.filter(entity -> !EntityUtil.basicChecksEntity(entity))
-				.filter(entity -> mc.player.getDistance(entity) <= range.getValue())
-				.filter(entity -> attackCheck(entity))
-				.sorted(Comparator.comparing(e -> mc.player.getDistance(e)))
+				.filter(entity -> mc.player.getDistanceSq(entity) <= rangeSq)
+				.filter(this::attackCheck)
+				.sorted(Comparator.comparing(e -> mc.player.getDistanceSq(e)))
 				.collect(Collectors.toList());
 
-		targets.forEach(target -> {
-			if (swordOnly.getValue() && !(mc.player.getHeldItemMainhand().getItem() instanceof ItemSword)) {
-				return;
+		if (shouldAttack()) {
+			// only attack one entity per cycle
+			Optional<Entity> first = targets.stream().findFirst();
+			if (first.isPresent()) {
+				if (autoSwitch.getValue() && (mc.player.getHealth() + mc.player.getAbsorptionAmount() >= switchHealth.getValue())) {
+					Pair<Float, Integer> newSlot = new Pair<>(0f, -1);
+
+					// find the best weapon in out hotbar
+					if (sword.getValue() || all.getValue()) {
+						newSlot = findSwordSlot();
+					}
+					if ((axe.getValue() || all.getValue()) && !(swordPriority.getValue() && newSlot.getValue() != -1)) {
+						Pair<Float, Integer> possibleSlot = findAxeSlot();
+						if (possibleSlot.getKey() > newSlot.getKey()) {
+							newSlot = possibleSlot;
+						}
+					}
+
+					// we have found a slot
+					if (newSlot.getValue() != -1) {
+						mc.player.inventory.currentItem = newSlot.getValue();
+					}
+				}
+
+				attack(first.get());
 			}
-			if (caCheck.getValue() && ((AutoCrystalGS) ModuleManager.getModuleByName("AutoCrystalGS")).isActive) {
-				return;
-			}
-			attack(target);
-		});
+		}
 	}
 
 	@EventHandler
@@ -87,7 +133,66 @@ public class KillAura extends Module {
 		GameSense.EVENT_BUS.unsubscribe(this);
 	}
 
-	public void attack(Entity e) {
+	private Pair<Float, Integer> findSwordSlot() {
+		List<Integer> items = InventoryUtil.findAllItemSlots(ItemSword.class);
+		List<ItemStack> inventory = mc.player.inventory.mainInventory;
+
+		float bestModifier = 0f;
+		int correspondingSlot = -1;
+		for (Integer integer : items) {
+			if (integer > 8) {
+				continue;
+			}
+
+			ItemStack stack = inventory.get(integer);
+			// generic best modifier
+			float modifier = (EnchantmentHelper.getModifierForCreature(stack, EnumCreatureAttribute.UNDEFINED) + 1f) * ((ItemSword)stack.getItem()).getAttackDamage();
+			// is it the new best
+			if (modifier > bestModifier) {
+				bestModifier = modifier;
+				correspondingSlot = integer;
+			}
+		}
+
+		return new Pair<>(bestModifier, correspondingSlot);
+	}
+
+	private Pair<Float, Integer> findAxeSlot() {
+		List<Integer> items = InventoryUtil.findAllItemSlots(ItemAxe.class);
+		List<ItemStack> inventory = mc.player.inventory.mainInventory;
+
+		float bestModifier = 0f;
+		int correspondingSlot = -1;
+		for (Integer integer : items) {
+			if (integer > 8) {
+				continue;
+			}
+
+			ItemStack stack = inventory.get(integer);
+			// generic best modifier
+			float modifier = (EnchantmentHelper.getModifierForCreature(stack, EnumCreatureAttribute.UNDEFINED) + 1f) * ((ItemAxe)stack.getItem()).attackDamage;
+			// is it the new best
+			if (modifier > bestModifier) {
+				bestModifier = modifier;
+				correspondingSlot = integer;
+			}
+		}
+
+		return new Pair<>(bestModifier, correspondingSlot);
+	}
+
+	private boolean shouldAttack() {
+		Item item = mc.player.getHeldItemMainhand().getItem();
+		if ((sword.getValue() && item instanceof ItemSword) ||
+				(axe.getValue() && item instanceof ItemAxe)
+				|| all.getValue()) {
+			return !caCheck.getValue() || !((AutoCrystalGS) ModuleManager.getModuleByName("AutoCrystalGS")).isActive;
+		}
+
+		return false;
+	}
+
+	private void attack(Entity e) {
 		if (mc.player.getCooledAttackStrength(0) >= 1) {
 			isAttacking = true;
 			mc.playerController.attackEntity(mc.player, e);
