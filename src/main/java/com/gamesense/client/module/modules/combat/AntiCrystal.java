bd170d08b087a26eb623e9514661960017753c65
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-02-15 23:00:40 -0500

    Replaced more usages of looking up module by string

--- src/main/java/com/gamesense/client/module/modules/combat/AntiCrystal.java
+++ src/main/java/com/gamesense/client/module/modules/combat/AntiCrystal.java
@@ -228,7 +228,7 @@ public class AntiCrystal extends Module {
 
         boolean stoppedAC = false;
 
-        if (ModuleManager.isModuleEnabled("AutoCrystalGS")) {
+        if (ModuleManager.isModuleEnabled(AutoCrystalGS.class)) {
             AutoCrystalGS.stopAC = true;
             stoppedAC = true;
         }
