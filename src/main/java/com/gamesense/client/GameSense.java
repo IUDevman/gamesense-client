b5bd8c9aa230bc78f2fc73a945826f07d3d52fa8
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-02-19 08:39:19 -0600

    version push

--- src/main/java/com/gamesense/client/GameSense.java
+++ src/main/java/com/gamesense/client/GameSense.java
@@ -29,7 +29,7 @@ public class GameSense {
 
 	public static final String MODNAME = "GameSense";
 	public static final String MODID = "gamesense";
-	public static final String MODVER = "v2.2.7";
+	public static final String MODVER = "d2.2.8";
 	/** Official release starts with a "v", dev versions start with a "d" to bypass version check */
 
 	public static final Logger LOGGER = LogManager.getLogger(MODNAME);
