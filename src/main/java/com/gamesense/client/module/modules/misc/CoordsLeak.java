b7e3947982ac90c9670cbc992996ec1025b94395
Doogie13 <62295632+Doogie13@users.noreply.github.com>
2021-02-24 16:35:58 +0000

    Added CoordLeak module that is very useful
    its not loading

---
+++ src/main/java/com/gamesense/client/module/modules/misc/CoordsLeak.java
@@ -0,0 +1,19 @@
+package com.gamesense.client.module.modules.misc;
+
+import com.gamesense.client.module.Module;
+import net.minecraft.entity.player.EntityPlayer;
+import java.util.Random;
+
+public class CoordsLeak extends Module {
+
+    public CoordsLeak() {
+        super("CoordsLeak", Category.Misc);
+    }
+
+    public void onEnable() {
+        if (mc.player != null) {
+            mc.player.sendChatMessage(">I just used the CoordsLeak module! Come grief my stash at" + (mc.player.posX) + ", " + (mc.player.posZ) + " thanks to GameSense!");
+            disable();
+        }
+    }
+}
