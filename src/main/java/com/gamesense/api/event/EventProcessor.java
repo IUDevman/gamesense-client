b07a8c32fc0a94e23bc1b9041ab0f06c18e1dbab
IUDevman <58370012+IUDevman@users.noreply.github.com>
2021-03-21 16:22:13 -0500

    Updated event posting

--- src/main/java/com/gamesense/api/event/EventProcessor.java
+++ src/main/java/com/gamesense/api/event/EventProcessor.java
@@ -111,6 +111,11 @@ public class EventProcessor {
         GameSense.EVENT_BUS.post(event);
     }
 
+    @SubscribeEvent
+    public void onTick(TickEvent.ClientTickEvent event) {
+        GameSense.EVENT_BUS.post(event);
+    }
+
     @EventHandler
     private final Listener<PacketEvent.Receive> receiveListener = new Listener<>(event -> {
         if (event.getPacket() instanceof SPacketPlayerListItem) {
