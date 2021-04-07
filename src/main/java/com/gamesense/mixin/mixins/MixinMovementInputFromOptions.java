2270062efe5154f60025f6e3a027e3f5d3db886e
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-04-06 23:07:35 -0400

    Fixed Gui Move mixin crash

--- src/main/java/com/gamesense/mixin/mixins/MixinMovementInputFromOptions.java
+++ src/main/java/com/gamesense/mixin/mixins/MixinMovementInputFromOptions.java
@@ -17,14 +17,18 @@ public abstract class MixinMovementInputFromOptions extends MovementInput {
 
     @Redirect(method = "updatePlayerMoveState", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/settings/KeyBinding;isKeyDown()Z"))
     public boolean isKeyPressed(KeyBinding keyBinding) {
-        PlayerTweaks playerTweaks = ModuleManager.getModule(PlayerTweaks.class);
+        int keyCode = keyBinding.getKeyCode();
 
-        if (playerTweaks.isEnabled() && playerTweaks.guiMove.getValue()
+        if (keyCode > 0 && keyCode < Keyboard.KEYBOARD_SIZE) {
+            PlayerTweaks playerTweaks = ModuleManager.getModule(PlayerTweaks.class);
+
+            if (playerTweaks.isEnabled() && playerTweaks.guiMove.getValue()
                 && Minecraft.getMinecraft().currentScreen != null
-                && !(Minecraft.getMinecraft().currentScreen instanceof GuiChat)
-                && Minecraft.getMinecraft().player != null) {
-            return Keyboard.isKeyDown(keyBinding.getKeyCode());
+                && !(Minecraft.getMinecraft().currentScreen instanceof GuiChat)) {
+                return Keyboard.isKeyDown(keyCode);
+            }
         }
+
         return keyBinding.isKeyDown();
     }
 }
