c21a5f0f779a09e177ad592bb96c2ad985ca429e
lukflug <lukflug@googlemail.com>
2021-04-09 20:14:41 +0200

    ReflectionUtil optimization

--- src/main/java/com/gamesense/api/util/misc/ReflectionUtil.java
+++ src/main/java/com/gamesense/api/util/misc/ReflectionUtil.java
@@ -3,6 +3,7 @@ package com.gamesense.api.util.misc;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -16,28 +17,37 @@ import com.gamesense.client.GameSense;
  */
 
 public class ReflectionUtil {
+	private static List<String> classes;
+	
     public static ArrayList<Class<?>> findClassesInPath(String classPath) {
+    	if (classes==null) {
+    		classes=new ArrayList<String>();
+    		String resource=ReflectionUtil.class.getResource(ReflectionUtil.class.getSimpleName()+".class").getPath();
+    		try {
+            	ZipInputStream file=new ZipInputStream(new URL(resource.substring(0,resource.lastIndexOf('!'))).openStream());
+            	ZipEntry entry;
+    			while ((entry=file.getNextEntry())!=null) {
+    				String name=entry.getName();
+    				if (name.endsWith(".class")) {
+    					classes.add(name.substring(0,name.length()-6).replace("/","."));
+    				}
+    			}
+    		} catch (IOException e) {
+    			e.printStackTrace();
+    		}
+    	}
     	GameSense.LOGGER.info("Loading classes from "+classPath+" ...");
         final ArrayList<Class<?>> foundClasses = new ArrayList<>();
-        String resource=ReflectionUtil.class.getClassLoader().getResource(classPath.replace(".","/")).getPath();
-        try {
-        	ZipInputStream file=new ZipInputStream(new URL(resource.substring(0,resource.lastIndexOf('!'))).openStream());
-        	ZipEntry entry;
-			while ((entry=file.getNextEntry())!=null) {
-				String name=entry.getName();
-				if (name.startsWith(classPath.replace(".","/")+"/") && name.endsWith(".class")) {
-					try {
-						Class<?> clazz=Class.forName(name.substring(0,name.length()-6).replace("/","."));
-						foundClasses.add(clazz);
-						System.out.println("Loaded "+clazz.getName()+"!");
-					} catch (ClassNotFoundException e) {
-						e.printStackTrace();
-					}
+        for (String className: classes) {
+        	if (className.startsWith(classPath+".")) {
+        		try {
+        			foundClasses.add(Class.forName(className));
+        			System.out.println("Loaded "+className+"!");
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
 				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        	}
+        }
         return foundClasses;
     }
 }
