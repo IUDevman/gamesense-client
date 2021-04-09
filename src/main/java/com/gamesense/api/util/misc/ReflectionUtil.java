7e139a0519932e4890f3e1ae9ca0d7f7f2551ba4
lukflug <lukflug@googlemail.com>
2021-04-09 19:03:19 +0200

    Made ReflectionUtil work

--- src/main/java/com/gamesense/api/util/misc/ReflectionUtil.java
+++ src/main/java/com/gamesense/api/util/misc/ReflectionUtil.java
@@ -1,55 +1,43 @@
 package com.gamesense.api.util.misc;
 
-import com.gamesense.client.GameSense;
-
-import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import com.gamesense.client.GameSense;
 
 /**
+ * Why not?
  * @author Hoosiers
  * @since 04/06/2021
+ * @author lukflug
  */
 
 public class ReflectionUtil {
-
     public static ArrayList<Class<?>> findClassesInPath(String classPath) {
+    	GameSense.LOGGER.info("Loading classes from "+classPath+" ...");
         final ArrayList<Class<?>> foundClasses = new ArrayList<>();
-
+        String resource=ReflectionUtil.class.getClassLoader().getResource(classPath.replace(".","/")).getPath();
         try {
-            //get the class loader from our main class
-            ClassLoader classLoader = GameSense.class.getClassLoader();
-
-            if (classLoader != null) {
-
-                URL classPathURL = classLoader.getResource(classPath.replace(".", "/"));
-
-                if (classPathURL != null) {
-
-                    //location of the files inside the jar
-                    File file = new File(classPathURL.getFile());
-
-                    if (file.exists()) {
-                        String[] classNamesFound = file.list();
-
-                        if (classNamesFound != null) {
-
-                            //we want to filter out other file types so we don't end up trying to load a .txt file
-                            for (String classNames : classNamesFound) {
-
-                                if (classNames.endsWith(".class")) {
-                                    foundClasses.add(Class.forName(classPath + "." + classNames.replace(".class", "")));
-                                    System.out.println(classNames);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        	ZipInputStream file=new ZipInputStream(new URL(resource.substring(0,resource.lastIndexOf('!'))).openStream());
+        	ZipEntry entry;
+			while ((entry=file.getNextEntry())!=null) {
+				String name=entry.getName();
+				if (name.startsWith(classPath.replace(".","/")+"/") && name.endsWith(".class")) {
+					try {
+						Class<?> clazz=Class.forName(name.substring(0,name.length()-6).replace("/","."));
+						foundClasses.add(clazz);
+						System.out.println("Loaded "+clazz.getName()+"!");
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
         return foundClasses;
     }
 }
