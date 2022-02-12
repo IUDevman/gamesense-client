52e59efa5bea6ccebf241a8a50f2e76fd8829f36
IUDevman <58370012+IUDevman@users.noreply.github.com>
2022-02-12 00:08:40 -0600

    Update CommandManager.java

--- src/main/java/dev/gamesense/client/manager/managers/CommandManager.java
+++ src/main/java/dev/gamesense/client/manager/managers/CommandManager.java
@@ -1,7 +1,12 @@
 package dev.gamesense.client.manager.managers;
 
 import dev.gamesense.GameSense;
+import dev.gamesense.client.command.Command;
 import dev.gamesense.client.manager.Manager;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author IUDevman
@@ -10,8 +15,65 @@ import dev.gamesense.client.manager.Manager;
 
 public final class CommandManager implements Manager {
 
+    private String prefix = "-";
+
+    private final ArrayList<Command> commands = new ArrayList<>();
+
     @Override
     public void load() {
         GameSense.INSTANCE.LOGGER.info("CommandManager");
+
+        getCommandsToLoad().forEach(this::addCommand);
+
+        postSortCommands();
+    }
+
+    private ArrayList<Command> getCommandsToLoad() {
+        ArrayList<Command> commands = new ArrayList<>();
+
+        return commands;
+    }
+
+    public void postSortCommands() {
+        this.commands.sort(Comparator.comparing(Command::getName));
+    }
+
+    public void addCommand(Command command) {
+        this.commands.add(command);
+    }
+
+    public ArrayList<Command> getCommands() {
+        return this.commands;
+    }
+
+    public Command getCommand(String name) {
+        return this.commands.stream().filter(command -> command.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
+    }
+
+    public String getPrefix() {
+        return this.prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        if (prefix.length() > 1) return;
+
+        this.prefix = prefix;
+    }
+
+    public void dispatchCommands(String message) {
+        String[] splitMessage = message.split("\\s");
+
+        AtomicBoolean foundMessage = new AtomicBoolean(false);
+
+        getCommands().forEach(command -> {
+            if (command.getName().equalsIgnoreCase(splitMessage[0])) {
+                foundMessage.set(true);
+                command.onCommand(splitMessage);
+            }
+        });
+
+        if (!foundMessage.get()) {
+            //todo: command error message
+        }
     }
 }
